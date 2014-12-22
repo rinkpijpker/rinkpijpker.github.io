8619b9056c983527a5bb1c1eef3eb1325978b27f
rinkpijpker <rinkpijpker@gmail.com>
2014-12-22 14:30:06 +0100

    White paper formulier met Mailchimp API call
    V1

---
+++ whitepaper/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>White paper formulier</title>
+</head>
+<body>
+
+<form action="form-validate.php" method="POST">
+	<input type="email" name="user-email" required>
+	<input type="submit" value="Verzenden">
+</form>
+	
+</body>
+</html>
