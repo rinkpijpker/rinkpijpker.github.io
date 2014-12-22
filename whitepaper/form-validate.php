91be9b71ecadf00f966d799c08ac978a4adb2674
rinkpijpker <rinkpijpker@gmail.com>
2014-12-22 14:32:12 +0100

    HTML tags toegevoegd aan het formulier

--- whitepaper/form-validate.php
+++ whitepaper/form-validate.php
@@ -1,14 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Whitepaper formulier</title>
+</head>
+<body>
 <?php 
 	$email = $_POST['user-email'];
 
+	include('mailchimp.php'); 
 
-include('mailchimp.php'); 
-
-
-$MailChimp = new \Drewm\MailChimp('d5598bd613ffadfd0410e503a1b7b633-us8');
-$result = $MailChimp->call('campaigns/create', array(
-               'id'					=> '514105',
-               'email'				=> $email,
-               'double_optin'		=> false
-            ));
+	$MailChimp = new \Drewm\MailChimp('d5598bd613ffadfd0410e503a1b7b633-us8');
+	$result = $MailChimp->call('campaigns/create', array(
+       'id'					=> '514105',
+       'email'				=> $email,
+       'double_optin'		=> false
+    ));
 ?>
+</body>
+</html>
